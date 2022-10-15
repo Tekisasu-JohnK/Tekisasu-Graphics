3eafee35c48af5b91eeaafa22ceaaaa3e56213d4
John Knight <80524176+Tekisasu-JohnK@users.noreply.github.com>
2022-10-15 16:14:38 -0500

    update thirdparty libaries

--- third_party/libarchive/libarchive/test/test_compat_tar_directory.c
+++ third_party/libarchive/libarchive/test/test_compat_tar_directory.c
@@ -1,7 +1,5 @@
 /*-
- * Copyright (c) 2016 IBM Corporation
- * Copyright (c) 2003-2007 Tim Kientzle
- *
+ * Copyright (c) 2021 Samanta Navarro
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -23,48 +21,57 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This test case's code has been derived from test_entry.c
  */
 #include "test.h"
+__FBSDID("$FreeBSD$");
 
-DEFINE_TEST(test_schily_xattr_pax)
+/*
+ * Background:  Original tar file format did not use its linkflag to
+ * specify directories.  Instead regular files simply have a slash
+ * appended to their names.  No data blocks follow directories in
+ * archives.  This means that a possibly specified file size must not
+ * be used to determine the amount of data blocks to skip.
+ */
+
+static void
+test_compat_tar_directory_1(void)
 {
-	struct archive *a;
+	char name[] = "test_compat_tar_directory_1.tar";
 	struct archive_entry *ae;
-	const char *refname = "test_read_pax_schily_xattr.tar";
-	const char *xname; /* For xattr tests. */
-	const void *xval; /* For xattr tests. */
-	size_t xsize; /* For xattr tests. */
-	const char *string, *array;
+	struct archive *a;
 
 	assert((a = archive_read_new()) != NULL);
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_filter_all(a));
 	assertEqualIntA(a, ARCHIVE_OK, archive_read_support_format_all(a));
+	extract_reference_file(name);
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_open_filename(a, name, 10240));
 
-	extract_reference_file(refname);
-	assertEqualIntA(a, ARCHIVE_OK,
-	    archive_read_open_filename(a, refname, 10240));
+	/* Read first entry, which is a directory in a regular file header. */
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualString("directory1/", archive_entry_pathname(ae));
+	assertEqualInt(AE_IFDIR, archive_entry_filetype(ae));
+	assertEqualInt(1, archive_entry_size(ae));
 
-	assertEqualInt(ARCHIVE_OK, archive_read_next_header(a, &ae));
-	assertEqualInt(2, archive_entry_xattr_count(ae));
-	assertEqualInt(2, archive_entry_xattr_reset(ae));
+	/* Read second entry, which is a ustar directory entry. */
+	assertEqualIntA(a, ARCHIVE_OK, archive_read_next_header(a, &ae));
+	assertEqualString("directory2/", archive_entry_pathname(ae));
+	assertEqualInt(AE_IFDIR, archive_entry_filetype(ae));
+	assertEqualInt(0, archive_entry_size(ae));
 
-	assertEqualInt(0, archive_entry_xattr_next(ae, &xname, &xval, &xsize));
-	assertEqualString(xname, "security.selinux");
-	string = "system_u:object_r:unlabeled_t:s0";
-	assertEqualString(xval, string);
-	/* the xattr's value also contains the terminating \0 */
-	assertEqualInt((int)xsize, strlen(string) + 1);
+	/* Verify the end-of-archive. */
+	assertEqualIntA(a, ARCHIVE_EOF, archive_read_next_header(a, &ae));
 
-	assertEqualInt(0, archive_entry_xattr_next(ae, &xname, &xval, &xsize));
-	assertEqualString(xname, "security.ima");
-	assertEqualInt((int)xsize, 265);
-	/* we only compare the first 12 bytes */
-	array = "\x03\x02\x04\xb0\xe9\xd6\x79\x01\x00\x2b\xad\x1e";
-	assertEqualMem(xval, array, 12);
+	/* Verify that the format detection worked. */
+	assertEqualInt(archive_filter_code(a, 0), ARCHIVE_FILTER_NONE);
+	assertEqualInt(archive_format(a), ARCHIVE_FORMAT_TAR);
 
-	/* Close the archive. */
 	assertEqualInt(ARCHIVE_OK, archive_read_close(a));
 	assertEqualInt(ARCHIVE_OK, archive_read_free(a));
 }
+
+DEFINE_TEST(test_compat_tar_directory)
+{
+	test_compat_tar_directory_1();
+}
+
+
